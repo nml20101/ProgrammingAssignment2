531c968b6f27468eab280e0afbf8316c90cf97c1
nml20101 <lacevic.n@gmail.com>
2014-05-25 11:33:22 -0700

    Updated programming assingment 2, checks if matrix is square

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,26 @@
 ## contains a function to set the value of the matrix, 
 ## get the value of the matrix, set the value of the inverse,
 ## and get the value of the inverse
+## Example to test the functions:
+## create matrix m1
+## > m1 <- matrix(c(1,2,3,4), nrow =2, ncol =2)
+## create special 'vector'
+## > m1_spec = makeCacheMatrix(m1)
+## attributes(m1_spec) 
+## > $names
+## > [1] "set"      "get"      "setsolve" "getsolve"
+## execute caseSolve
+## > cacheSolve(m1_spec)
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## execute caseSolve again
+## > cacheSolve(m1_spec)
+## getting cached data
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > 
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -12,7 +32,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 m <<- NULL
         }
-        # need to check if matrix is square
+                
         get <- function() x
         setsolve <- function(solve) m <<- solve
         getsolve <- function() m
@@ -27,10 +47,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
+    
         m <- x$getsolve()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
+        }
+        # check if the matrix is square and its determinat is not zero
+        dim1 <- dim(x$get())[1]
+        dim2 <- dim(x$get())[2]
+        if (( dim1!= dim2 ) | (det(data) != 0 )) {
+                print("Matrix is not invertible")
+                stop
         }
         data <- x$get()
         m <- solve(data, ...)
